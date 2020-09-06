0d136aa71a8291da3fe83bc91008e13e7248af24
Michael Horsley <omega_mike@hotmail.com>
2020-09-06 21:41:43 +0100

    Green | Apply discount and charge extra at full price

--- CheapestPriceTests/DiscountService.cs
+++ CheapestPriceTests/DiscountService.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace CheapestPriceTests
 {
     public class DiscountService
     {
-        public decimal CalculatorPrices(List<Discount> discounts,List<int> list)
+        public decimal CalculatorPrices(List<Discount> discounts, List<int> list)
         {
-            return list.Count * 8.0m;
+            var sumTotal = 0m;
+
+            foreach (var discount in discounts.OrderByDescending(x => x.SetCount))
+            {
+                if (list.Count == discount.SetCount)
+                {
+                    sumTotal += 8.0m * list.Count * (100.0m - discount.DiscountPercentage) / 100.0m;
+                    
+                    for (int i = 0; i < discount.SetCount; i++)
+                    {
+                        list.RemoveAt(0);
+                    }
+                }
+            }
+
+            sumTotal += list.Count * 8.0m;
+
+            return sumTotal;
         }
     }
 
