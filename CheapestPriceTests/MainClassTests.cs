def87156f2f9b1870a9e35c27d0d3dac4fe090f9
Michael Horsley <omega_mike@hotmail.com>
2020-09-06 21:05:32 +0100

    End goal test with wrong solution

---
+++ CheapestPriceTests/MainClassTests.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace CheapestPriceTests
+{
+    [TestFixture]
+    public class MainClassTests
+    {
+        [Test]
+        public void FinalGoal()
+        {
+            var discountService = new DiscountService();
+
+            decimal calculatorPrices = discountService.CalculatorPrices(new List<int> { 1, 1, 2, 2, 3, 3, 4, 5 });
+
+            Assert.AreEqual(new decimal(51.20), calculatorPrices);
+        }
+    }
+}
