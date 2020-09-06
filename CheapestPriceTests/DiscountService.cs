7bcf4535d01c3751269fdaa945229a688ee5b78d
Michael Horsley <omega_mike@hotmail.com>
2020-09-06 21:13:40 +0100

    Failing | Added Discount object to store set number and percentage

--- CheapestPriceTests/DiscountService.cs
+++ CheapestPriceTests/DiscountService.cs
@@ -4,9 +4,15 @@ namespace CheapestPriceTests
 {
     public class DiscountService
     {
-        public decimal CalculatorPrices(List<int> list)
+        public decimal CalculatorPrices(List<Discount> discounts,List<int> list)
         {
             return list.Count * 8.0m;
         }
+    }
+
+    public class Discount
+    {
+        public int SetCount { get; set; }
+        public int DiscountPercentage { get; set; }
     }
 }
--- CheapestPriceTests/MainClassTests.cs
+++ CheapestPriceTests/MainClassTests.cs
@@ -6,12 +6,20 @@ namespace CheapestPriceTests
     [TestFixture]
     public class MainClassTests
     {
+        private readonly List<Discount> _discounts = new List<Discount>
+        {
+            new Discount
+            {
+                SetCount = 2, DiscountPercentage = 5
+            }
+        };
+
         [Test]
         public void FinalGoal()
         {
             var discountService = new DiscountService();
 
-            decimal calculatorPrices = discountService.CalculatorPrices(new List<int> { 1, 1, 2, 2, 3, 3, 4, 5 });
+            decimal calculatorPrices = discountService.CalculatorPrices(_discounts, new List<int> { 1, 1, 2, 2, 3, 3, 4, 5 });
 
             Assert.AreEqual(new decimal(51.20), calculatorPrices);
         }
@@ -21,7 +29,7 @@ namespace CheapestPriceTests
         {
             var discountService = new DiscountService();
 
-            decimal calculatorPrices = discountService.CalculatorPrices(new List<int> { 1 });
+            decimal calculatorPrices = discountService.CalculatorPrices(_discounts, new List<int> { 1 });
 
             Assert.AreEqual(new decimal(8.0), calculatorPrices);
         }
@@ -31,7 +39,7 @@ namespace CheapestPriceTests
         {
             var discountService = new DiscountService();
 
-            decimal calculatorPrices = discountService.CalculatorPrices(new List<int> { 1, 2 });
+            decimal calculatorPrices = discountService.CalculatorPrices(_discounts, new List<int> { 1, 2 });
 
             Assert.AreEqual(new decimal(15.2), calculatorPrices);
         }
