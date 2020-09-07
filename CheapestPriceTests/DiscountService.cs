22a87c69ebd9d37065dfedc4c00e489abe8495d5
Michael Horsley <omega_mike@hotmail.com>
2020-09-07 02:21:01 +0100

    Green | Calculated Cheapest Price by worked through the list in order of combinations of discounts applied

--- CheapestPriceTests/DiscountService.cs
+++ CheapestPriceTests/DiscountService.cs
@@ -7,17 +7,45 @@ namespace CheapestPriceTests
     {
         public decimal CalculateCheapestPrice(List<Discount> discounts, List<int> list)
         {
+            var discountCombinationPrices = new List<decimal>();
+
+            var orderByDescending = discounts.OrderByDescending(x => x.SetCount).ToList();
+
+            while (orderByDescending.Any())
+            {
+                discountCombinationPrices.Add(GetDiscountCombination(orderByDescending, list));
+
+                orderByDescending.Remove(orderByDescending.First());
+            }
+
+            return discountCombinationPrices.Where(x => !x.Equals(0m)).Min();
+        }
+
+        private static decimal GetDiscountCombination(List<Discount> discounts, List<int> list)
+        {
+            var discountsOrderedByDescendingRequiredNumber = discounts.OrderByDescending(x => x.SetCount).ToList();
+            
             var sumTotal = 0m;
 
             var productListToBeAltered = new List<int>(list);
 
-            foreach (var discount in discounts.OrderByDescending(x => x.SetCount))
+            if (!RemoveValidProductsFromList(new List<int>(productListToBeAltered), discountsOrderedByDescendingRequiredNumber.First()))
             {
-                var validForDiscount = ValidForDiscount(productListToBeAltered, discount);
+                return list.Count * 8m;
+            }
+            
+            while (discountsOrderedByDescendingRequiredNumber.Any())
+            {
+                var discount = discountsOrderedByDescendingRequiredNumber.First();
+                var validForDiscount = RemoveValidProductsFromList(productListToBeAltered, discount);
 
                 if (validForDiscount)
                 {
                     sumTotal += 8.0m * discount.SetCount * (100.0m - discount.DiscountPercentage) / 100.0m;
+                }
+                else
+                {
+                    discountsOrderedByDescendingRequiredNumber.Remove(discount);
                 }
             }
 
@@ -26,14 +54,13 @@ namespace CheapestPriceTests
             return sumTotal;
         }
 
-        private static bool ValidForDiscount(List<int> productListToBeAltered, Discount discount)
+        private static bool RemoveValidProductsFromList(List<int> productListToBeAltered, Discount discount)
         {
             var productHashSet = new HashSet<int>();
             var itemsAddedToDiscount = new List<int>();
 
-            for (var index = productListToBeAltered.Count - 1; index >= 0; index--)
+            foreach (var product in productListToBeAltered)
             {
-                var product = productListToBeAltered[index];
                 var added = productHashSet.Add(product);
 
                 if (added)
